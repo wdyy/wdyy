1627b311ce2d481aa85a6bb513d75b606d905e7a
FanRuiQi <3193765359@qq.com>
2019-01-29 20:30:43 +0800

    frq - 20:30

--- app/src/main/java/com/bw/movie/fragment/CinemaPopupCommentFragment.java
+++ app/src/main/java/com/bw/movie/fragment/CinemaPopupCommentFragment.java
@@ -1,18 +1,26 @@
 package com.bw.movie.fragment;
 
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.bw.movie.R;
+import com.bw.movie.apis.Apis;
 import com.bw.movie.base.BaseFragment;
+import com.bw.movie.bean.CinemaCommentBean;
 import com.bw.movie.bean.CinemaPopupDetailsBean;
+import com.bw.movie.fragment.adapter.CinemaCommentAdapter;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Author: 范瑞旗
@@ -23,6 +31,7 @@ public class CinemaPopupCommentFragment extends BaseFragment {
 
     @BindView(R.id.fragment_cinema_recommend_popupWindow_comment_recyclerView)
     RecyclerView mRecyclerView;
+    private int mCinemaId;
 
     @Override
     public void initView(View view) {
@@ -35,13 +44,24 @@ public class CinemaPopupCommentFragment extends BaseFragment {
         ButterKnife.bind(this,view);
         EventBus.getDefault().register(this);
 
+
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN,sticky = true)
     public void sjx(CinemaPopupDetailsBean detailsBean){
 
+         mCinemaId = detailsBean.getId();
+         doNetRequestData(String.format(Apis.URL_CINEMA_All_COMMENT,mCinemaId),null,CinemaCommentBean.class,"get");
 
     }
+
+    @OnClick(R.id.fragment_cinema_recommend_popupWindow_comment_img_comment)
+    public void onImgCommentClickListener(){
+
+
+    }
+
+
 
     @Override
     public int getContent() {
@@ -51,6 +71,17 @@ public class CinemaPopupCommentFragment extends BaseFragment {
     @Override
     public void success(Object data) {
 
+        if (data instanceof CinemaCommentBean){
+            CinemaCommentBean cinemaCommentBean = (CinemaCommentBean) data;
+            List<CinemaCommentBean.ResultBean> result = cinemaCommentBean.getResult();
+
+            CinemaCommentAdapter adapter = new CinemaCommentAdapter(getActivity(), result);
+            mRecyclerView.setAdapter(adapter);
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false));
+
+            mRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(),DividerItemDecoration.VERTICAL));
+
+        }
     }
 
     @Override
--- app/src/main/java/com/bw/movie/general/activity/SuccessActivity.java
+++ app/src/main/java/com/bw/movie/general/activity/SuccessActivity.java
@@ -3,8 +3,10 @@ package com.bw.movie.general.activity;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bw.movie.R;
 import com.bw.movie.base.BaseActivity;
@@ -32,6 +34,8 @@ public class SuccessActivity extends BaseActivity {
     ImageView mSuccess_button_mines;
     private FragmentManager mManager;
 
+    private long exitTime = 0;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +45,27 @@ public class SuccessActivity extends BaseActivity {
 
     @Override
     public void initView() {
+
+    }
+
+    // 用来计算返回键的点击间隔时间
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK
+                && event.getAction() == KeyEvent.ACTION_DOWN) {
+            if ((System.currentTimeMillis() - exitTime) > 2000) {
+                //弹出提示，可以有多种方式
+                Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+            }
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
 
     }
 
